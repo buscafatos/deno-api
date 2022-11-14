3b4b9ebf4ddba27bcc7349b3ff32eaecf8e801ee
fcavalcantirj <felipe.cavalcanti.rj@gmail.com>
2022-11-14 02:49:23 -0300

    funcionando via rest

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import {google} = from 'googleapis';
-const customsearch = google.customsearch('v1');
+import url from  "https://esm.sh/url";
 
 const SEARCH_ENGINE_ID = Deno.env.get("SEARCH_ENGINE_ID");
 const API_KEY = Deno.env.get("API_KEY");
 
-serve((_req) => {
 
-  // return new Response("Hello World!", {
-  //   headers: { "content-type": "text/plain" },
-  // });
+function handler(_req) {
 
-    customsearch.cse.list({
-      cx: SEARCH_ENGINE_ID,
-      q: _req.params.q,
-      auth: API_KEY,
-    }, function(res){
-      return res.data;
-    });
+  const queryObject = url.parse(_req.url, true).query;
+  console.log(queryObject.q);
 
-});
+  let _url = 'https://www.googleapis.com/customsearch/v1?key='+API_KEY+'&cx='+SEARCH_ENGINE_ID+'&q='+queryObject.q;
+  console.log('url =' + _url);
+
+  const request = new Request(_url, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+
+  return fetch(request);
+}
+
+serve(handler);
