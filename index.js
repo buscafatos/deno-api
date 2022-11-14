a6bf4f8b7508522167a33b04e0e520b988bdbdae
fcavalcantirj <felipe.cavalcanti.rj@gmail.com>
2022-11-13 23:33:44 -0300

    teste google search

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+const {google} = require('googleapis');
+const customsearch = google.customsearch('v1');
+
+const SEARCH_ENGINE_ID = '9145e8c50ed1b4c5a'
+const API_KEY = 'AIzaSyBKURzCt4zi4iQ1Indcf007Ws_-2avnoCk'
 
 serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+  // return new Response("Hello World!", {
+  //   headers: { "content-type": "text/plain" },
+  // });
+  
+  const res = await customsearch.cse.list({
+    cx: SEARCH_ENGINE_ID,
+    q: _req.params.q,
+    auth: API_KEY,
   });
+  return res.data;
 });
