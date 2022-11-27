42f507d2d10ee1025ba9a56b7973b5bb67a5c739
fcavalcantirj <felipe.cavalcanti.rj@gmail.com>
2022-11-27 02:17:39 -0300

    synching

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ import { Application, Router, isHttpError } from "https://deno.land/x/oak/mod.ts
 import url from  "https://esm.sh/url";
 import { transform } from "https://esm.sh/node-json-transform";
 
-const SEARCH_ENGINE_ID = Deno.env.get("SEARCH_ENGINE_ID");
-const API_KEY = Deno.env.get("API_KEY");
+const SEARCH_ENGINE_ID = Deno.env.get("SEARCH_ENGINE_ID_1");
+const API_KEY = Deno.env.get("API_KEY_1");
 
 
 let baseMap = {
