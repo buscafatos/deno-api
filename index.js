b033af2e6b5657b3cb03abd1cccc0bff1f0b6492
fcavalcantirj <felipe.cavalcanti.rj@gmail.com>
2022-11-27 01:22:55 -0300

    synching

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ router
   });
 
   function errorHandler(error) {
-    if(error.error && !error.error.includes('UnexpectedEof: early eof')) {
+    if(error.error && error.error.indexOf('UnexpectedEof') > 0) {
       // console.log('error');
       console.log(error.error);
     }
